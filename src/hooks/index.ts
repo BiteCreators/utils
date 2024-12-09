05bffbfc2ac94ff8deeebf935d270388996cbb51
tapuru <pogladtapira@gmail.com>
2024-12-09 13:51:17 +0500

    fix: fix index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,5 +3,4 @@ export { useHandleLimitedInput } from "./useHandleLimitedInput";
 export { useLoader } from "./useLoader";
 export { useMediaQuery } from "./useMediaQuery";
 export { useShowMore } from "./useShowMore";
-// export { useScopedTranslation, useTranslation } from './useTranslation'
 export { useValidationLimit } from "./useValidationLimit";
