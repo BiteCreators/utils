8d27f0f43173311039454465c69fd252e575828c
DePacos <depacos@yandex.ru>
2024-12-18 19:15:12 +0300

    added useTextArea

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export { useConfirmation } from "./useConfirmaion";
 export { useGetRelativeTime } from "./useGetRelativeTime";
 export { useHandleLimitedInput } from "./useHandleLimitedInput";
 export { useLoader } from "./useLoader";
+export { useTextArea } from "./useTextArea";
 export { useMediaQuery } from "./useMediaQuery";
 export { useShowMore } from "./useShowMore";
 export { useScopedTranslation, useTranslation } from "./useTranslation";
