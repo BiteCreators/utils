7463cd9e1131b4839f0ef38cf63fd105706f3234
fireonex <mari.krulikovskaya@mail.ru>
2024-12-20 12:27:20 +0300

    add useIntersectionObserver

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,12 @@
-export { useConfirmation } from "./useConfirmaion";
-export { useDebounce } from "./useDebounce";
-export { useGetRelativeTime } from "./useGetRelativeTime";
-export { useHandleLimitedInput } from "./useHandleLimitedInput";
-export { useLoader } from "./useLoader";
-export { useMediaQuery } from "./useMediaQuery";
-export { useShowMore } from "./useShowMore";
-export { useTextArea } from "./useTextArea";
-export { useScopedTranslation, useTranslation } from "./useTranslation";
-export { useValidationLimit } from "./useValidationLimit";
-export { setCanvasPreview } from "./setCanvasPreview";
+export {useConfirmation} from "./useConfirmaion";
+export {useDebounce} from "./useDebounce";
+export {useGetRelativeTime} from "./useGetRelativeTime";
+export {useHandleLimitedInput} from "./useHandleLimitedInput";
+export {useLoader} from "./useLoader";
+export {useMediaQuery} from "./useMediaQuery";
+export {useShowMore} from "./useShowMore";
+export {useTextArea} from "./useTextArea";
+export {useScopedTranslation, useTranslation} from "./useTranslation";
+export {useValidationLimit} from "./useValidationLimit";
+export {setCanvasPreview} from "./setCanvasPreview";
+export {useIntersectionObserver} from "./useIntersectionObserver"
