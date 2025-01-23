f9f9f9194eff8a05dc60b4fe749b8c23c8d1213d
DmitryChues <dmitrychues2312@gmail.com>
2025-01-23 03:09:23 +0300

    add translation to admin-user-profile locales

--- src/locales/admin-user-profile/admin-user-profile.ru.ts
+++ src/locales/admin-user-profile/admin-user-profile.ru.ts
@@ -1,13 +1,14 @@
 export const adminUserProfileRu = {
-    back: 'Назад к списку пользователей',
-    userID: 'ID пользователя',
-    errors: {
-        userNotFound: 'Пользователь не найден',
-    },
-    profileCreationDate: 'Дата создания профиля',
-    uploadedPhotos: 'Загруженные фотографии',
-    payments: 'Платежи',
-    followers: 'Подписчики',
-    following: 'Подписки',
-    noPhotos: 'Нет загруженных фотографий',
-}
+  back: "Назад к списку пользователей",
+  userID: "ID пользователя",
+  errors: {
+    userNotFound: "Пользователь не найден",
+    noItemsFound: "Элемент не найден",
+  },
+  profileCreationDate: "Дата создания профиля",
+  uploadedPhotos: "Загруженные фотографии",
+  payments: "Платежи",
+  followers: "Подписчики",
+  following: "Подписки",
+  noPhotos: "Нет загруженных фотографий",
+};
