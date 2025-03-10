fcadf19636b45927f8784812b6debd38fd35f6da
DmitryChues <dmitrychues2312@gmail.com>
2025-03-10 16:21:25 +0300

    added export base-query

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,3 +3,4 @@ export { formatPaymentMethod } from "./formatPaymentMethod";
 export { formatSubscriptionType } from "./formatSubscriptionType";
 export { removeParam } from "./removeParam";
 export { wordWrapping } from "./wordWrapping";
+export { baseQueryWithReauth } from "./base-query"
